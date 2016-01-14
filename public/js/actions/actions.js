ae9152a724658c584ca24a1264d8fb0128607435
httpNick <nick.b.duncan@gmail.com>
2016-01-14 01:23:55 -0800

    Modifications to how the DB object is passed around. Some more refactoring.

--- public/js/actions/actions.js
+++ public/js/actions/actions.js
@@ -4,20 +4,14 @@ import {constants} from '../constants/constants';
 
 module.exports = {
 
-  getTest : () => {
-    AppDispatcher.dispatch({
-      actionType : constants.TEST
-    });
-    api.search();
-  },
-
   searchForTerm : (term) => {
-    api.searchRequest(term, (data) => {
+
+    api.searchRequest(term, (err, data) => {
       AppDispatcher.dispatch({
         actionType: constants.TEST
       });
-      console.log(data);
     });
+
   }
 
 };
--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-var express = require('express');
-var app = express();
-var router = require('./server/route/router.js');
+var establishdb = require('./server/utils/establishdbconnection');
+establishdb((err, db) => {
+  if (err) return;
+  var express = require('express');
+  var app = express();
+  var router = require('./server/route/router.js')(db);
 
-app.engine('html', require('ejs').renderFile);
+  app.engine('html', require('ejs').renderFile);
 
 // use statements
-app.use('/', express.static(__dirname + '/public'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-app.use('/', router);
+  app.use('/', express.static(__dirname + '/public'));
+  app.use('/bower_components', express.static(__dirname + '/bower_components'));
+  app.use('/', router);
 
 // set statements
-app.set('view engine', 'html');
-app.set('views', __dirname + '/public/view');
+  app.set('view engine', 'html');
+  app.set('views', __dirname + '/public/view');
 
 // get statements
-app.get('/', (req, res) => {
-  res.render('index');
-});
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
 
-var server = app.listen(3000, () => {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('listening at http://%s:%s', host, port);
+  var server = app.listen(3000, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('listening at http://%s:%s', host, port);
+
+    server.on('close', () => {
+      db.close();
+    });
+
+  });
 });
