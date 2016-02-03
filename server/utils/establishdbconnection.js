357ae22e754c5a5bc024f290c5c885d33f651980
httpNick <nick.b.duncan@gmail.com>
2016-02-02 21:37:15 -0800

    updates for azure.

--- server/utils/establishdbconnection.js
+++ server/utils/establishdbconnection.js
@@ -3,7 +3,7 @@ var mongoclient = require('mongodb').MongoClient
 
 module.exports = (cb) => {
 
-	mongoclient.connect(dbconfig.url, (err, db) => {
+	mongoclient.connect('mongodb://httpnick:Passeword6@ds060968.mongolab.com:60968/csgo-cachedb', (err, db) => {
 		if (err) cb(err, null);
 		cb(null, db);
 	});
