3f6b7d5cb71be623d761169ceaf252768ac7fc15
httpNick <nick.b.duncan@gmail.com>
2016-02-02 21:46:23 -0800

    Added process port to listener.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ establishdb((err, db) => {
     res.render('index');
   });
 
-  var server = app.listen(3000, () => {
+  var server = app.listen(process.env.PORT, () => {
     var host = server.address().address;
     var port = server.address().port;
     console.log('listening at http://%s:%s', host, port);
