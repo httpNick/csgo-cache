148c1c9f5e5b0e6e971fb3a8dd0b62e11adbe9c0
httpNick <nick.b.duncan@gmail.com>
2016-02-03 03:45:52 -0800

    some changes to insert scripts. need to implement timeout feature to stop the 429s.

--- scripts/insertskins.js
+++ scripts/insertskins.js
@@ -1,16 +1,58 @@
 var dbhandle = require('../server/utils/dbhandler')
 	, establishdb = require('../server/utils/establishdbconnection')
-	, csgomarket = require('csgo-market');
+	, csgomarket = require('csgo-market')
+	, allItems = require('./items');
 
-const testSkin = {
+var itemInConsideration = allItems[0];
+var wears = [
+	'Factory New', 'Minimal Wear',
+	'Field-Tested', 'Well-Worn',
+	'Battle-Scarred'
+];
 
-	item     : 'AK-47',
-	skin     : 'Vulcan',
-	wear     : 'Field-Tested',
-	stattrak : false
+establishdb((err, db) => {
 
-};
+	var testSkin = {
 
+		item: itemInConsideration.name,
+		skin: '',
+		wear: '',
+		stattrak: false
+
+	};
+
+	for(var x = 0; x < itemInConsideration.skins.length; x++) {
+		testSkin.skin = itemInConsideration.skins[x].skinName;
+		for (var i = 0; i < wears.length; i++) {
+			testSkin.wear = wears[i];
+			csgomarket.getSinglePrice(
+				testSkin.item,
+				testSkin.skin,
+				testSkin.wear,
+				testSkin.stattrak,
+				(err, data) => {
+					if (err) {
+						console.dir(err);
+					} else {
+						dbhandle.insertSkin(
+							data,
+							db,
+							(err, r) => {
+								if (!err) {
+									console.log(r);
+								} else {
+									console.log(err);
+								}
+							}
+						)
+					}
+				}
+			)
+		}
+	}
+});
+
+/*
 establishdb((err, db) => {
 	if (!err) {
 		csgomarket.getSinglePrice(
@@ -33,3 +75,4 @@ establishdb((err, db) => {
 		);
 	}
 });
+	*/
